821a2411a11eaa1c185a6bd32e43550840e0bbc3
tharun.s <tharun.s@corefactors.in>
2025-03-09 22:32:32 +0530

    added notes model

--- Dealer/models.py
+++ Dealer/models.py
@@ -1,2 +1,10 @@
 from django.db import models
+from ProductManagement.models import Lead
+from UserManagement.models import CustomUser
 
+class LeadNotes(models.Model):
+    dealer              = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    lead                = models.ForeignKey(Lead, blank=True, null=True, on_delete=models.CASCADE)
+    notes               = models.TextField(blank=True, null=True)
+    created_at          = models.DateTimeField(auto_now_add=True, verbose_name = "Created At", null=True, blank=True)
+    modified_at         = models.DateTimeField(auto_now=True, verbose_name = "Last Modified At", null=True, blank=True)
